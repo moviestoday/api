987cb58035d6e968f5de76c5708218c52118e736
Bishaka Samuel <sbishaka@gmail.com>
2015-12-11 15:06:46 +0300

    Close #8 - Add codename to /cinemas endpoint

--- core/api.js
+++ core/api.js
@@ -6,8 +6,8 @@ var
 api.getCinemas = function(){
     return new Promise(function(resolve, reject){
         var cinemas = [
-            {"id":"1","name":"Century Cinemax","location":{ "name":"Acacia Mall" }},
-            {"id":"2","name":"3D Cinema Magic","location":{ "name":"Metroplex Mall" }}
+            {"id":"1",codename:"cinemax","name":"Century Cinemax","location":{ "name":"Acacia Mall" }},
+            {"id":"2",codename:"cinemagic","name":"3D Cinema Magic","location":{ "name":"Metroplex Mall" }}
         ];
         resolve(cinemas);
     })
